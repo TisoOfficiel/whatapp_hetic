6a9097220f8d0f88dde22a228c9e99d96b7dc750
Romain Lhuillier <51294979+TisoOfficiel@users.noreply.github.com>
2023-10-08 18:11:28 +0200

    Add files via upload

---
+++ src/assets/data/messageAnswer.js
@@ -0,0 +1,8 @@
+export const MessageAnswer = [
+    {message_content: 'Bonjour',date: new Date(), status:'recipient'},
+    {message_content: 'Comment vas-tu ?',date: new Date(), status:'recipient'},
+    {message_content: 'Bonjour',date: new Date(), status:'recipient'},
+    {message_content: 'Quoi de neuf ?',date: new Date(), status:'recipient'},
+    {message_content: 'Enchanté de vous rencontrer',date: new Date(), status:'recipient'},
+    {message_content: 'Bonne journée',date: new Date(), status:'recipient'},
+];
---
+++ src/assets/data/messages.js
@@ -0,0 +1,5 @@
+export const Messages = [
+    {message_id: 1, message_content: "Salut !"},
+    {message_id: 2, message_content: "Comment ça va ?"},
+    {message_id: 3, message_content: "Bien et toi"},
+]
